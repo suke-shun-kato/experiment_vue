91979cb2c4e4d0413636787e81436e6f3f839533
KatoShunsuke <suke.shun.kato2@gmail.com>
2023-05-02 17:29:32 +0900

    ログイン用のページを作成

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    const eMail = ref('')
+    const password = ref('')
+
+    function login() {
+        console.log(eMail.value)
+        console.log(password.value)
+    }
+</script>
+
+<template>
+    <div>
+        <!-- デフォルト-->
+        <form @submit.prevent="login">
+            <div>
+                <label for="e-mail">Email address</label>
+                <input type="email" id="e-mail" v-model.trim="eMail" />
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" v-model.trim="password" />
+            </div>
+            <div>
+                <input type="submit" value="ログイン" />
+            </div>
+        </form>
+    </div>
+</template>
+
