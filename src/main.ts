3a91d320167faefa99f8ed236a50346543f79d4b
KatoShunsuke <suke.shun.kato2@gmail.com>
2023-05-09 11:34:26 +0900

    VueRouterで正常に動くように修正

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './views/LoginView.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 import { createPinia } from 'pinia'
 
 import './assets/main.css'
