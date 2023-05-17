d7f860870bf270f7f66d45752c1acd1d6436494e
KatoShunsuke <suke.shun.kato2@gmail.com>
2023-05-17 11:46:50 +0900

    バリデーションエラー処理を関数にまとめた

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,18 +8,16 @@ import type {LoginReqest} from "@/api/requestParams/LoginReqest";
 import type {RouteLocation} from "vue-router";
 import { useRoute, useRouter} from "vue-router";
 import RunDisabledButton from "@/components/RunDisabledButton.vue";
-import axios from "axios";
 import {NoAuthApiService} from "@/api/service/NoAuthApiService";
 import type {LoginErrorResponse} from "@/api/errorResponseParams/LoginErrorResponse";
 import ValidationError from "@/components/ValidationError.vue";
-import {ErrorResponseStatusCodes} from "@/api/StatusCode";
-import type {StatusCode} from "@/api/StatusCode";
+import {executeFormApi} from "@/api/utils/executeFormApi";
 
 // const eMail: Ref<string> = ref('suke.shun.kato2@gmail.com')
 const eMail: Ref<string> = ref('')
 // const password: Ref<string> = ref('password')
 const password: Ref<string> = ref('')
-const errorRef: Ref<LoginErrorResponse|undefined> = ref()
+const errorResponseRef: Ref<LoginErrorResponse|undefined> = ref()
 const router = useRouter()
 const route = useRoute()
 const apiService = new NoAuthApiService()
@@ -30,7 +28,8 @@ const login = async (): Promise<void> => {
         email: eMail.value,
         password: password.value
     }
-    try {
+
+    await executeFormApi<LoginErrorResponse>(errorResponseRef, async (): Promise<void> => {
         const response: AxiosResponse = await apiService.login(loginReqParam)
         console.log(response)
 
@@ -42,41 +41,24 @@ const login = async (): Promise<void> => {
         // ログイン成功したとみなして、リダイレクト
         const fromRouteLocation: RouteLocation|undefined = route.redirectedFrom // リダイレクト元のロケーションを取得
         await router.push(fromRouteLocation?.fullPath ?? '/')   // リダイレクト元へリダイレクト（fromRouteLocation が undefined のときは '/'）
-    } catch (e: any) {
-        console.error(e)
-        if (axios.isAxiosError(e)) {
-            if (e.response !== undefined
-                && ErrorResponseStatusCodes.includes(e.response?.status as StatusCode)) {
-            // 認証エラー、バリデーションエラー
-                errorRef.value = e.response?.data
-            } else {
-                errorRef.value = {
-                    message: e.message
-                }
-            }
-        } else {
-            errorRef.value = {
-                message: 'runtime error'
-            }
-        }
 
-    }
+    })
 }
 </script>
 
 <template>
     <div>
         <form>
-            <div v-if="errorRef">{{ errorRef.message }}</div>
+            <div v-if="errorResponseRef">{{ errorResponseRef.message }}</div>
             <div>
                 <label for="e-mail">Email address</label>
                 <input type="email" id="e-mail" v-model.trim="eMail" />
-                <ValidationError :error-messages="errorRef?.errors?.email"/>
+                <ValidationError :error-messages="errorResponseRef?.errors?.email"/>
             </div>
             <div>
                 <label for="password">Password</label>
                 <input type="password" id="password" v-model.trim="password" />
-                <ValidationError :error-messages="errorRef?.errors?.password" />
+                <ValidationError :error-messages="errorResponseRef?.errors?.password" />
             </div>
             <div>
                 <RunDisabledButton :onClick="login">ログイン</RunDisabledButton>
